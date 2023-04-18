ac4ccee21f27d46d2509e54f11694b0d3c142420
Shengrong Xu <shengrx@ggg.eecs.umich.edu>
2023-04-18 00:33:24 -0400

    fixed assignRank

--- mypass/PRE.cpp
+++ mypass/PRE.cpp
@@ -20,91 +20,139 @@
 // 1. Safe-Earliest Transformation: insert h = t at every entry of node n satisfying DSafe & Earliest and replace each t by h
 // (1) down-safe (DSafe): computation of t at n does not introduce a new value on a terminating path starting in n.
 // (2) Earliest: path from start to end, no node m prior to n is safe and delivers the same value as n when computing t.
-// 
+//
 //  Suppress Unnecessary Code Motion
 // 2. Latest Transformation: insert h = t at every entry of node n satisfying Delay & Latest and replace each t by h
 // (3) Delay (from DSafe and Earliest) --> Latest: values
-// 
+//
 //  LCM Transformatio
 // 3. Optimal Transformation
 // (4) Isolated & Latest --> OCP; RO --> insert h = t at every entry of node n in OCP and replace each t by h in RO.
 
 using namespace llvm;
 using namespace std;
-namespace{
-	struct PRE: public FunctionPass{
+namespace
+{
+    struct PRE : public FunctionPass
+    {
         static char ID;
-        //FOR legacy LLVM
-		PRE() : FunctionPass(ID) {
+        // FOR legacy LLVM
+        PRE() : FunctionPass(ID)
+        {
         }
-        virtual bool runOnFunction(Function &F) override{
+        std::unordered_map<llvm::Value *, int> rankMap;
+        virtual bool runOnFunction(Function &F) override
+        {
             // DominatorTree DT = DominatorTree(F);
             // PromoteMemToReg(F.getParent()->getDataLayout()).runOnFunction(F);
+            assignRank(F);
+            for (const auto &kv : rankMap)
+            {
+                llvm::Value *key = kv.first;
+                int value = kv.second;
 
+                // Assuming you have access to the LLVM context, you can use the following
+                // code to print the key (llvm::Value*) in a human-readable format
+                key->print(llvm::errs(), false); // Set the second parameter to true if you want to print the type as well
+                llvm::errs() << " -> " << value << "\n";
+            }
+            return false;
         }
-
 
         // Assume that 'F' is a pointer to the function for which you want to construct
         // the SSA form with phi nodes.
-        
+
         // This function takes a pointer to a function and converts it to SSA form with phi nodes.
 
+        // For LLVM 4.0
+        //  PreservedAnalyses run(Function &F, FunctionAnalysisManager &FAM) {
 
-        //For LLVM 4.0
-        // PreservedAnalyses run(Function &F, FunctionAnalysisManager &FAM) {
-            
         //     FunctionPassManager function_pass_manager;
-
 
         //     function_pass_manager.addPass(ReassociatePass());
         //     function_pass_manager.run(F, FAM);
 
         //     return PreservedAnalyses::none();
         // }
-        std::vector<BasicBlock*> getFrequentPath(Function *F) {
+        std::vector<BasicBlock *> getFrequentPath(Function *F)
+        {
             BranchProbabilityInfo &bpi = getAnalysis<BranchProbabilityInfoWrapperPass>().getBPI();
-            
+
             // Find the entry block
-            BasicBlock* entryBB = &F->getEntryBlock();
+            BasicBlock *entryBB = &F->getEntryBlock();
             std::vector<BasicBlock *> frequentPath;
             BasicBlock *BB = entryBB;
-            while (true){
+            while (true)
+            {
                 BasicBlock *next = nullptr;
-                for (succ_iterator PI = succ_begin(BB); PI != succ_end(BB); ++PI) {
+                for (succ_iterator PI = succ_begin(BB); PI != succ_end(BB); ++PI)
+                {
                     next = *PI;
                     BranchProbability currProInfo = bpi.getEdgeProbability(BB, next);
-                    float currPro = (float)currProInfo.getNumerator()/(float)currProInfo.getDenominator();
-                    if (currPro >= 0.8){
+                    float currPro = (float)currProInfo.getNumerator() / (float)currProInfo.getDenominator();
+                    if (currPro >= 0.8)
+                    {
                         break;
                     }
                 }
                 frequentPath.push_back(next);
-                if (next == entryBB){
+                if (next == entryBB)
+                {
                     break;
                 }
                 BB = next;
             }
             return frequentPath;
         }
-        std::unordered_map<llvm::Value*,int> rankMap;
-        void assignRank(Function &F) {
-            for (auto &BB : F) {
-                for (auto &I : BB){
-                    if (llvm::StoreInst *SI = &llvm::dyn_cast<llvm::StoreInst>(I)){
+
+        void assignRank(Function &F)
+        {
+            int bb_num = 1;
+            for (auto &BB : F)
+            {
+
+                for (auto &I : BB)
+                {
+                    // if (llvm::StoreInst *SI = &llvm::dyn_cast<llvm::StoreInst>(I)){
+                    //     llvm::Value *op_SI = SI->getOperand(1);
+                    //     if (llvm::Constant *CI = &llvm::dyn_cast<llvm::Constant>(*op_SI)){
+                    //         rankMap[SI->getOperand(0)] = 0;
+                    //     }
+
+                    // }
+                    // else if (llvm::PHINode *PHI = &llvm::dyn_cast<llvm::PHINode>(I)){
+                    //     rankMap[PHI->getOperand(0)] = bb_num;
+                    // }
+                    // else if (llvm::BinaryOpIntrinsic *BI = &llvm::dyn_cast<llvm::BinaryOpIntrinsic>(I)){
+                    //     llvm::Value *op_SI_l = BI->getOperand(1);
+                    //     llvm::Value *op_SI_r = BI->getOperand(2);
+                    //     rankMap[BI->getOperand(0)] = max(rankMap[op_SI_l],rankMap[op_SI_r]);
+
+                    // }
+                    if (llvm::isa<llvm::StoreInst>(I))
+                    {
+                        llvm::StoreInst *SI = llvm::cast<llvm::StoreInst>(&I);
                         llvm::Value *op_SI = SI->getOperand(1);
-                        if (llvm::Constant *CI = &llvm::dyn_cast<llvm::Constant>(*op_SI)){
+                        if (llvm::isa<llvm::Constant>(*op_SI))
+                        {
+                            llvm::Constant *CI = llvm::cast<llvm::Constant>(op_SI);
                             rankMap[SI->getOperand(0)] = 0;
                         }
-                    
                     }
-                    else if (llvm::BinaryOpIntrinsic *BI = &llvm::dyn_cast<llvm::BinaryOpIntrinsic>(I)){
-                        llvm::Value *op_SI_l = BI->getOperand(1);
-                        llvm::Value *op_SI_r = BI->getOperand(2);
-                        rankMap[BI->getOperand(0)] = max(rankMap[op_SI_l],rankMap[op_SI_r]);
-
+                    else if (llvm::isa<llvm::PHINode>(I))
+                    {
+                        llvm::PHINode *PHI = llvm::cast<llvm::PHINode>(&I);
+                        rankMap[PHI->getOperand(0)] = bb_num;
                     }
-
+                    else if (llvm::isa<llvm::BinaryOperator>(I))
+                    {
+                        llvm::BinaryOperator *BI = llvm::cast<llvm::BinaryOperator>(&I);
+                        llvm::Value *op_SI_l = BI->getOperand(0);
+                        llvm::Value *op_SI_r = BI->getOperand(1);
+                        rankMap[BI->getOperand(0)] = std::max(rankMap[op_SI_l], rankMap[op_SI_r]);
+                    }
                 }
+                bb_num += 1;
             }
         }
 
@@ -115,7 +163,8 @@ namespace{
         //  (1) Remove each Phi node x = Phi(y, z) by inserting the copies x = y and x = z
         //  (2) Trace from each copy back along the SSA graph (new blocks required) to construct expression trees
         //  (3) Check the uses and push expressions
-        bool forwardProp(Function &F) {
+        bool forwardProp(Function &F)
+        {
             // // Create a map to store the uses of Phi nodes
             // std::map<PHINode *, std::vector<User *>> PhiUsesMap;
             // // Position of Phi nodes
@@ -143,5 +192,5 @@ namespace{
 }
 char PRE::ID = 0;
 static RegisterPass<PRE> X("PRE", "PRE pass",
-    false /* Only looks at CFG */,
-    false /* Analysis Pass */);
+                           false /* Only looks at CFG */,
+                           false /* Analysis Pass */);
